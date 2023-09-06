a917d1a0f3cb9764b31d85a811c82a0a7f4ec1eb
abel.santos <abel.santos@arxes-tolina.de>
2023-09-06 09:14:54 +0200

    Changed code to match Udemy course

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo"/>
           <p>
-            test
+            Welcome to React
           </p>
           <a
               className="App-link"
@@ -15,7 +15,7 @@ function App() {
               target="_blank"
               rel="noopener noreferrer"
           >
-            Learn react  dwad
+            Learn react
           </a>
         </header>
       </div>
